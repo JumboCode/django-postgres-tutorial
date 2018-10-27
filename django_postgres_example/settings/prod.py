a92e46ee7a5414edb38ed7d0f4ceb63c615edc56
Spencer Perry <sperry@Spencers-MacBook-Air.local>
2018-10-27 09:45:06 -0400

    turn debug back to false

--- django_postgres_example/settings/prod.py
+++ django_postgres_example/settings/prod.py
@@ -10,7 +10,7 @@ import dj_database_url
 # We don't want any debug warnings giving
 # away unnecessary information to attackers
 
-DEBUG=True
+DEBUG=False
 
 # We grab the secret key from the environment because it is
 # our production key and no can know it
