64003fe4a2871f633e3db426ba305daa3d875f2b
Spencer Perry <sperry@Spencers-MacBook-Air.local>
2018-10-26 22:52:10 -0400

    commented settings added travis file, should fail

--- django_postgres_example/settings/dev.py
+++ django_postgres_example/settings/dev.py
@@ -4,15 +4,22 @@
 
 from .common import *
 
+DEBUG = True
+ALLOWED_HOSTS = ['localhost']
+
+# It's okay to have secret key in plain text since it is
+# different then the production environment key
+
+SECRET_KEY = 'm&%ij7#cpxk$p2w(@%u#7s)_$@!%)#fgp&^7q&0id1b4(fg(o&'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-ALLOWED_HOSTS = ['localhost']
-SECRET_KEY = 'm&%ij7#cpxk$p2w(@%u#7s)_$@!%)#fgp&^7q&0id1b4(fg(o&'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-DEBUG = True
+# The necessary information for connecting to the
+# the postgres database running on our local machine
 
 DATABASES = {
     'default': {
--- django_postgres_example/settings/prod.py
+++ django_postgres_example/settings/prod.py
@@ -7,16 +7,33 @@ from .common import *
 import dj_database_url
 
 
+# We don't want any debug warnings giving
+# away unnecessary information to attackers
+
 DEBUG=False
-SECURE_SSL_REDIRECT = True
 
+# We grab the secret key from the environment because it is
+# our production key and no can know it
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# We redirect any http requests to their  https equivalents
+
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ["jcdjangopostgres.herokuapp.com"]
 
-DATABASES = { }
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# In a real production environment, we would likely want to
+# handle static files on a different machine.
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# We let the dj_database_url package pull the database info from heroku
+# https://github.com/kennethreitz/dj-database-url
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
